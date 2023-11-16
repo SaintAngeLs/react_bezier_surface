5f54e67dcebabbec4b0a440e9b8e6fced70c2823
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-17 00:36:42 +0100

    Besier surface update using the Bershtain polynomial

--- src/core/BezierSurface.tsx
+++ src/core/BezierSurface.tsx
@@ -5,41 +5,111 @@ import * as THREE from 'three';
 import { useThree, useFrame } from '@react-three/fiber';
 
 // Function to generate control points for the Bezier surface
-const generateControlPoints = (size: number): THREE.Vector3[] => {
-    const controlPoints: THREE.Vector3[] = [];
-    for (let i = 0; i < size; i++) {
-      // Push new Vector3 with random coordinates into the array
-      controlPoints.push(new THREE.Vector3(Math.random(), Math.random(), Math.random()));
+const generateControlPoints = (): THREE.Vector3[][] => {
+  const controlPoints: THREE.Vector3[][] = [];
+  for (let i = 0; i < 4; i++) {
+    controlPoints[i] = [];
+    for (let j = 0; j < 4; j++) {
+      const z = -((i - 1.5) ** 2) - (j - 1.5) ** 2 + 4;
+      controlPoints[i].push(new THREE.Vector3(i - 1.5, j - 1.5, z));
     }
-    return controlPoints;
-  };
-  
+  }
+  return controlPoints;
+};
+
+// Bernstein polynomial
+const B = (i: number, n: number, t: number): number => {
+  return binomialCoefficient(n, i) * t ** i * (1 - t) ** (n - i);
+};
+
+// Binomial coefficient
+const binomialCoefficient = (n: number, k: number) => {
+  let result = 1;
+  for (let i = 1; i <= k; i++) {
+    result *= (n + 1 - i) / i;
+  }
+  return result;
+};
+
 
 // Function to evaluate a point on a Bezier surface given control points and u, v parameters
 const evaluateBezierSurface = (
-    u: number,
-    v: number,
-    controlPoints: THREE.Vector3[]
-  ): THREE.Vector3 => {
-    // Implement the actual Bezier surface point calculation here
-    // Placeholder for demonstration
-    return new THREE.Vector3(u, v, Math.sin(u) * Math.cos(v));
-  };
+  u: number,
+  v: number,
+  controlPoints: THREE.Vector3[][]
+): THREE.Vector3 => {
+  let point = new THREE.Vector3(0, 0, 0);
+  const n = controlPoints.length - 1;
 
-  
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      const bernsteinU = B(i, n, u);
+      const bernsteinV = B(j, n, v);
+      const controlPoint = controlPoints[i][j];
+
+      point.x += bernsteinU * bernsteinV * controlPoint.x;
+      point.y += bernsteinU * bernsteinV * controlPoint.y;
+      point.z += bernsteinU * bernsteinV * controlPoint.z;
+    }
+  }
+
+  return point;
+};
+
+
+// Function to create vertices for a Bezier surface based on control points
+const createBezierVertices = (
+  accuracy: number,
+  controlPoints: THREE.Vector3[][]
+): Float32Array => {
+  const vertices: any = [];
+  for (let i = 0; i <= accuracy; i++) {
+    const u = i / accuracy;
+    for (let j = 0; j <= accuracy; j++) {
+      const v = j / accuracy;
+      const point = evaluateBezierSurface(u, v, controlPoints);
+      vertices.push(point.x, point.y, point.z);
+    }
+  }
+  return new Float32Array(vertices);
+};
+
 // Function to create the geometry of the Bezier surface
 const createBezierGeometry = (
     accuracy: number,
-    controlPoints: THREE.Vector3[]
+    controlPoints: THREE.Vector3[][]
   ): THREE.BufferGeometry => {
     
-    const width = 100;
-    const height = 100;
+    const width = 40;
+    const height = 40;
     const widthSegments = accuracy - 1;
     const heightSegments = accuracy - 1;
+
+    const vertices = createBezierVertices(accuracy, controlPoints);
+
+    const indices: number[] = [];
+    for (let i = 0; i < accuracy; i++) {
+      for (let j = 0; j < accuracy; j++) {
+        const a = i * (accuracy + 1) + j;
+        const b = a + accuracy + 1;
+        const c = a + 1;
+        const d = b + 1;
+
+        indices.push(a, b, c);
+        indices.push(b, d, c);
+      }
+    }
+
+  
+    const geometry = new THREE.BufferGeometry();
+    geometry.setIndex(indices);
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.computeVertexNormals();
+  
+    return geometry;
   
     // Correctly named PlaneBufferGeometry is used to create the geometry
-    const geometry = new THREE.PlaneGeometry(width, height, widthSegments, heightSegments);
+    //const geometry = new THREE.PlaneGeometry(width, height, widthSegments, heightSegments);
 
   
     // // Get a reference to the position attribute
@@ -59,34 +129,44 @@ const createBezierGeometry = (
     //   positionAttribute.setZ(i, zValue);
     // }
 
-    const positions = geometry.attributes.position.array;
+  //   const positions = geometry.attributes.position.array;
 
-    // Loop through the position array and update the z values
-    for (let i = 0; i < positions.length; i += 3) {
-      const u = positions[i] + 0.5;
-      const v = positions[i + 1] + 0.5;
-      const point = evaluateBezierSurface(u, v, controlPoints);
-      positions[i + 2] = point.z; // Update the z component based on the Bezier surface
-    }
+  //   // Loop through the position array and update the z values
+  //   for (let i = 0; i < positions.length; i += 3) {
+  //     const u = positions[i] + 0.5;
+  //     const v = positions[i + 1] + 0.5;
+  //     const point = evaluateBezierSurface(u, v, controlPoints);
+  //     positions[i + 2] = point.z; // Update the z component based on the Bezier surface
+  //   }
   
-    // Tell three.js that the position attribute needs to be updated on the GPU
-   // positionAttribute.needsUpdate = true;
-    geometry.attributes.position.needsUpdate = true;
+  //   // Tell three.js that the position attribute needs to be updated on the GPU
+  //  // positionAttribute.needsUpdate = true;
+  //   geometry.attributes.position.needsUpdate = true;
 
   
-    // Compute vertex normals if necessary
-    geometry.computeVertexNormals();
+  //   // Compute vertex normals if necessary
+  //   geometry.computeVertexNormals();
   
-    return geometry;
+  //   return geometry;
   };
   
+interface BezierSurfaceProps {
+  accuracy: number;
+  texture?: THREE.Texture;
+  normalMap?: THREE.Texture;
+  kd: number;
+  ks: number;
+  specularExponent: number;
+  lightColor: string;
+}
 
-const BezierSurface = ({ accuracy, texture, normalMap, kd, ks, specularExponent, lightColor }) => {
-  const { gl } = useThree();
-  const controlPoints = generateControlPoints(16); // Assuming a 4x4 grid of control points
-
-  // Create the geometry
-  const geometry = useMemo(() => createBezierGeometry(accuracy, controlPoints), [accuracy, controlPoints]);
+const BezierSurface: React.FC<BezierSurfaceProps> = ({ accuracy, texture, normalMap, kd, ks, specularExponent, lightColor }) => {
+  const meshRef = React.useRef<THREE.Mesh>(null);
+  const controlPoints = generateControlPoints();
+  const geometry = useMemo(() => createBezierGeometry(accuracy, controlPoints), [
+    accuracy,
+    controlPoints,
+  ]);
 
   // Define shaders
   const vertexShader = `
@@ -150,13 +230,23 @@ const BezierSurface = ({ accuracy, texture, normalMap, kd, ks, specularExponent,
     },
   }), [kd, ks, specularExponent, lightColor]);
 
+  // const meshRef = React.useRef();
+  // const { gl, camera } = useThree();
+  // const controlPoints = generateControlPoints();
+
+  // const geometry = useMemo(() => createBezierGeometry(accuracy, controlPoints), [accuracy, controlPoints]);
+
   // Update the material on frame if necessary
-  useFrame(() => {
+  useFrame(({clock}) => {
     // Update uniforms or perform any animations
+
+    if (meshRef.current) {
+      meshRef.current.rotation.z = clock.getElapsedTime() * 0.1;
+    }
   });
 
   return (
-    <mesh geometry={geometry} material={material}>
+    <mesh ref={meshRef}  geometry={geometry} material={material}>
       {/* Add children or additional elements if needed */}
     </mesh>
   );
